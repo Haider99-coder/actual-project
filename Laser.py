65c66932769ec3d6c4e942bb5166810635e3048e
Haider99-coder <153817359+Haider99-coder@users.noreply.github.com>
2024-08-30 01:51:44 +0100

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -11,3 +11,41 @@ class Laser(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y += self.speed
+        class Boss(pygame.sprite.Sprite):
+    def __init__(self,side,screen_width):
+        super().__init__()
+        self.image = pygame.image.load ('sprites/blue_boss.png').convert_alpha()
+            
+        if side == 'right': # if boss spawns in right hand screen it moves to the left
+            x = screen_width + 50
+            self.speed = -3
+        else:
+            x = screen_width - 50
+            self.speed = 3
+
+        self.rect = self.image.get_rect(topleft =(x,300))
+
+
+class Extra(pygame.sprite.Sprite):
+    def __init__(self, side, screen_width):
+        super().__init__()
+        self.image = pygame.image.load('sprites/blue_boss.png').convert_alpha()  
+        if side == 'right':
+            x = screen_width + 50
+            self.speed = -5  # Adjust speed as needed
+        else:
+            x = -50
+            self.speed = 5
+
+        self.rect = self.image.get_rect(topleft=(x, 120))
+
+    def update(self):
+        self.rect.x += self.speed
+        
+
+
+    
+
+   #14iter to write in word docu
+   # def update(self,movement):
+    #self.rect.x += movement
