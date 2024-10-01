7e703e27f5579dfe626f3e4e8d03bf4261a1e822
Haider99-coder <153817359+Haider99-coder@users.noreply.github.com>
2024-10-01 16:31:31 +0100

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -2,50 +2,23 @@ import pygame
 
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self,pos,speed = -7):
+      def __init__(self,pos,speed = -7 ,screen_height= 800):
         super().__init__()
-        self.image = pygame.Surface((4,20)) # draws surface
+        self.image = pygame.Surface((7,20)) # draws surface
         self.image.fill('blue') # fill with white as the screen is black
         self.rect = self.image.get_rect(center = pos) # sets out position of the rectangle of laser
         self.speed = speed
+        self.height_y_constraint = screen_height
 
-    def update(self):
+      def destroy(self):
+        if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint + 50:
+           self.kill()
+
+      def update(self):
         self.rect.y += self.speed
-        class Boss(pygame.sprite.Sprite):
-    def __init__(self,side,screen_width):
-        super().__init__()
-        self.image = pygame.image.load ('sprites/blue_boss.png').convert_alpha()
-            
-        if side == 'right': # if boss spawns in right hand screen it moves to the left
-            x = screen_width + 50
-            self.speed = -3
-        else:
-            x = screen_width - 50
-            self.speed = 3
+        self.destroy()
 
-        self.rect = self.image.get_rect(topleft =(x,300))
-
-
-class Extra(pygame.sprite.Sprite):
-    def __init__(self, side, screen_width):
-        super().__init__()
-        self.image = pygame.image.load('sprites/blue_boss.png').convert_alpha()  
-        if side == 'right':
-            x = screen_width + 50
-            self.speed = -5  # Adjust speed as needed
-        else:
-            x = -50
-            self.speed = 5
-
-        self.rect = self.image.get_rect(topleft=(x, 120))
-
-    def update(self):
-        self.rect.x += self.speed
-        
 
 
     
 
-   #14iter to write in word docu
-   # def update(self,movement):
-    #self.rect.x += movement
